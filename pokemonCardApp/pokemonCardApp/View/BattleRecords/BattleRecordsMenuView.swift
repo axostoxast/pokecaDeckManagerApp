24153b14c3b000318720e5d36d3842797d80a68a
sota.suzuki <sota.suzuki@persol.co.jp>
2022-06-06 16:43:34 +0900

    対戦成績メニューを追加

---
+++ pokemonCardApp/pokemonCardApp/View/BattleRecords/BattleRecordsMenuView.swift
@@ -0,0 +1,44 @@
+//
+//  BattleRecordsMenuView.swift
+//  pokemonCardApp
+//
+//  Created by 鈴木綜太 on 2022/06/06.
+//
+
+import SwiftUI
+
+struct BattleRecordsMenuView: View {
+    
+    @State private var isShowRegisterRecord = false
+    @State private var isShowRecordList = false
+    
+    // Localizationから多言語取得
+    private let register: String = NSLocalizedString("RegisterRecord", comment: "RegisterRecord")
+    private let list: String = NSLocalizedString("RecordList", comment: "RecordList")
+    
+    init() {
+        UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color("basic"))]
+    }
+    
+    var body: some View {
+        ZStack{
+            NavigationLink(destination:AddDeckView(), isActive: $isShowRegisterRecord) {
+                EmptyView()
+            }
+            
+            NavigationLink(destination:DeckListView(), isActive: $isShowRecordList) {
+                EmptyView()
+            }
+        
+            VStack(spacing: 0) {
+                // 対戦成績記録
+                ButtonView(buttonName: register, isShowFlg: $isShowRegisterRecord)
+                
+                // 対戦成績一覧
+                ButtonView(buttonName: list, isShowFlg: $isShowRecordList)
+            }
+        }
+        .navigationTitle("Records")
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
--- pokemonCardApp/pokemonCardApp/View/TopView.swift
+++ pokemonCardApp/pokemonCardApp/View/TopView.swift
@@ -14,6 +14,13 @@ struct TopView: View {
     @State private var isShowAddDeck = false
     @State private var isShowMyCollection = false
     @State private var isShowMyFavoriteCollection = false
+    @State private var isShowRecords = false
+    
+    // Localizationから多言語取得
+    private let add: String = NSLocalizedString("Add", comment: "Add")
+    private let list: String = NSLocalizedString("List", comment: "List")
+    private let favorite: String = NSLocalizedString("Favorite", comment: "Favorite")
+    private let records: String = NSLocalizedString("Records", comment: "Records")
     
     init() {
         UINavigationBar.appearance().titleTextAttributes = [.foregroundColor: UIColor(Color("basic"))]
@@ -33,67 +40,49 @@ struct TopView: View {
                 NavigationLink(destination:FavoriteDeckListView(), isActive: $isShowMyFavoriteCollection) {
                     EmptyView()
                 }
+                
+                NavigationLink(destination:BattleRecordsMenuView(), isActive: $isShowRecords) {
+                    EmptyView()
+                }
             
                 VStack(spacing: 0) {
                     // デッキ登録
-                    Group {
-                        Button("Add") {
-                            isShowAddDeck = true
-                        }
-                        .font(.system(size: 20))
-                        .foregroundColor(Color("basic"))
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 3, trailing: 0))
-                        
-                        Divider()
-                            .frame(width: UIScreen.main.bounds.width * 0.5, height: 3, alignment: .center)
-                            .background(Color("blueLine"))
-                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 3, trailing: 0))
-                        Divider()
-                            .frame(width: UIScreen.main.bounds.width * 0.5, height: 3, alignment: .center)
-                            .background(Color("yellowLine"))
-                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 50, trailing: 0))
-                    }
+                    ButtonView(buttonName: add, isShowFlg: $isShowAddDeck)
                     
                     // デッキリスト
-                    Group {
-                        Button("List") {
-                            isShowMyCollection = true
-                        }
-                        .font(.system(size: 20))
-                        .foregroundColor(Color("basic"))
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 3, trailing: 0))
-                        
-                        Divider()
-                            .frame(width: UIScreen.main.bounds.width * 0.5, height: 3, alignment: .center)
-                            .background(Color("blueLine"))
-                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 3, trailing: 0))
-                        Divider()
-                            .frame(width: UIScreen.main.bounds.width * 0.5, height: 3, alignment: .center)
-                            .background(Color("yellowLine"))
-                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 50, trailing: 0))
-                    }
+                    ButtonView(buttonName: list, isShowFlg: $isShowMyCollection)
                     
                     // お気に入りリスト
-                    Group {
-                        Button("Favorite") {
-                            isShowMyFavoriteCollection = true
-                        }
-                        .font(.system(size: 20))
-                        .foregroundColor(Color("basic"))
-                        .padding(EdgeInsets(top: 0, leading: 0, bottom: 3, trailing: 0))
-                        
-                        Divider()
-                            .frame(width: UIScreen.main.bounds.width * 0.5, height: 3, alignment: .center)
-                            .background(Color("blueLine"))
-                            .padding(EdgeInsets(top: 0, leading: 0, bottom: 3, trailing: 0))
-                        Divider()
-                            .frame(width: UIScreen.main.bounds.width * 0.5, height: 3, alignment: .center)
-                            .background(Color("yellowLine"))
-                    }
+                    ButtonView(buttonName: favorite, isShowFlg: $isShowMyFavoriteCollection)
+
+                    // 対戦成績メニュー
+                    ButtonView(buttonName: records, isShowFlg: $isShowRecords)
                 }
             }
         }
-        .navigationTitle("ポケカアプリ")
     }
 }
 
+struct ButtonView: View {
+    
+    var buttonName: String
+    @Binding var isShowFlg: Bool
+    
+    var body: some View {
+        Button(buttonName) {
+            isShowFlg = true
+        }
+        .font(.system(size: 20))
+        .foregroundColor(Color("basic"))
+        .padding(EdgeInsets(top: 0, leading: 0, bottom: 3, trailing: 0))
+        
+        Divider()
+            .frame(width: UIScreen.main.bounds.width * 0.5, height: 3, alignment: .center)
+            .background(Color("blueLine"))
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 3, trailing: 0))
+        Divider()
+            .frame(width: UIScreen.main.bounds.width * 0.5, height: 3, alignment: .center)
+            .background(Color("yellowLine"))
+            .padding(EdgeInsets(top: 0, leading: 0, bottom: 50, trailing: 0))
+    }
+}
